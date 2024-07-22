b5d87a00d521c0fe30e54e9789dc5f580f41eb73
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2024-07-22 18:20:25 -0300

    Crete route createProducts

--- src/functions/createProduct.ts
+++ src/functions/createProduct.ts
@@ -1,11 +1,38 @@
 import { PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { APIGatewayAuthorizerEvent } from "aws-lambda";
+import { randomUUID } from "node:crypto";
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+import { client } from "../lib/dynamoClient";
 
-exports.index = async (event: APIGatewayAuthorizerEvent) => {
+exports.handler = async (event: APIGatewayProxyEventV2) => {
+  let body;
+
+  // Verifique se event.body não é undefined
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Request body is missing or empty",
+      }),
+    };
+  }
+  const id = randomUUID();
+  const command = new PutItemCommand({
+    TableName: "ProductsTable",
+    Item: {
+      id: { S: id },
+      name: { S: body.name },
+      description: { S: body.description },
+      price: { N: body.price.toString() },
+    },
+  });
+  const response = await client.send(command);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: event,
+      message: response,
     }),
   };
 };
